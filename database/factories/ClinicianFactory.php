ce5b188099b03c0c1767b30869ad0df151699488
Azhar Azziz <azharazziz13@gmail.com>
2023-03-21 19:31:07 +0700

    add model migrator and seeder for Clinician

---
+++ database/factories/ClinicianFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Clinician>
+ */
+class ClinicianFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+            'doctor_name' => fake()->name('male'),
+        ];
+    }
+}
